25d858e6d6c191269784e2b5287b7c5089d71524
walden <weidf@163.com>
2016-03-29 23:36:36 +0800

    first commit

---
+++ DesignPattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DesignPattern
+//
+//  Created by Walden on 16/3/29.
+//  Copyright © 2016年 Walden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
